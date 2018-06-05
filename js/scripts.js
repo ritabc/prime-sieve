6c126673385dc6086ee746e915e065cd25e40743
Guest User <Guest@Epicodus-7C.local>
2018-06-05 16:25:05 -0700

    Complete functionality (prints to console)

--- js/scripts.js
+++ js/scripts.js
@@ -7,46 +7,25 @@ function createArray(highNumber) {
 };
 
 function findPrimes() {
-  var number = 100;
-  var myArray = createArray(number)
+  var highNumber = 100;
+  var myArray = createArray(highNumber)
 
-  // loop should eventually start here
-
-  // var prime = 2;
-  // // for now, remove all multiples of prime (which is currently 2)
-  // // by looping through primeList
-  //
-  // for (var i = prime; i <= primeList.length + 1; i += 1) {
-  // var prime = primeList[0];
-  //
-  //   // we need a new index j, which goes 0, 1, 2, 3, with the array elements
-  //   for (j = 0; j <= primeList.length; ++j) {
-  //     primeList.splice(j, 1)
-  //   };
-  //   console.log(primeList)
-  //
-  // // for (var i = prime; i <= primeList.length + 1; ++i)
-  // // console.log(i);
-  // // console.log(primeList);
-  // // primeList.splice()
-  // };
-
-  // for (i = myArray[0]; i <= myArray.length + 1; i++) {
-  myArray.map(function(number, index) {
-    // remove all elements which are multiples of myArray[0] (which first is 2)
-    if ((number % myArray[0] === 0) && (number !== myArray[0])) {
-      myArray.splice(index, 1);
-    // that will include myArray[0], so it needs to be added back, and since we do this in the loop we need to reverse outside of the loop
-    }
-  });
-
-
-  // now myArray = [2,3,5,7,9,11,13,15,17]
-  myArray.map(function(number, index) {
-    if ((number % myArray[1] === 0) && (number !== myArray[1])) {
-    myArray.splice(index, 1)
-    };
-  });
+  for (i = 0; i <= highNumber; ++i) {
+    myArray.map(function(number, index) {
+      // remove all elements which are multiples of myArray[0] (which first is 2)
+      if ((number % myArray[i] === 0) && (number !== myArray[i])) {
+        myArray.splice(index, 1);
+      // that will include myArray[0], so it needs to be added back, and since we do this in the loop we need to reverse outside of the loop
+      };
+    });
+    //
+    // // now myArray = [2,3,5,7,9,11,13,15,17]
+    // myArray.map(function(number, index) {
+    //   if ((number % myArray[i] === 0) && (number !== myArray[i])) {
+    //   myArray.splice(index, 1)
+    //   };
+    // });
+  }
   console.log(myArray);
 
 
